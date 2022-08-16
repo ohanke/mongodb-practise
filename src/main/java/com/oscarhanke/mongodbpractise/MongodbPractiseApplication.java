2f37869420ccb45a46fcebcef08befc3555ea054
ohanke <steamoha@wp.pl>
2022-08-16 19:50:35 +0200

    Initial commit.

---
+++ src/main/java/com/oscarhanke/mongodbpractise/MongodbPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.oscarhanke.mongodbpractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbPractiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oscarhanke/mongodbpractise/MongodbPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarhanke.mongodbpractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
