9eddb7c4c8e827d8a28579a28590603b4f0446c3
ohanke <steamoha@wp.pl>
2022-08-17 00:06:06 +0200

    made email unique

--- src/main/java/com/oscarhanke/mongodbpractise/Student.java
+++ src/main/java/com/oscarhanke/mongodbpractise/Student.java
@@ -2,6 +2,7 @@ package com.oscarhanke.mongodbpractise;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
@@ -17,6 +18,7 @@ public class Student {
     private String id;
     private String firstName;
     private String lastName;
+    @Indexed(unique = true)
     private String email;
     private Gender gender;
     private Adress adress;
